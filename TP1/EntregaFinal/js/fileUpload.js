3c84a0b47ee2713a4c36ae628367d10da4895384
Axel Bautista <axelbau24@gmail.com>
2017-08-21 22:23:28 -0300

    Agregado efectos en el diseño

--- TP1/EntregaFinal/js/fileUpload.js
+++ TP1/EntregaFinal/js/fileUpload.js
@@ -1,16 +1,45 @@
-$('#imageUpload').on('change',function(){
+const SELECTED_IMAGE_THUMBNAIL = "#selectedImage";
+const CANVAS_CONTAINER = ".image-container";
 
-  let file =  document.getElementById('imageUpload').files[0];
+$('#imageUpload').on('change', function () {
+  loadImage($(this));
+
+  $(CANVAS_CONTAINER).hide("slide", 500, slideCanvas);
+});
+
+$("#firstUpload").on("change", function() {
+  loadImage($(this));
+
+  let home = $(".home-container").effect( "puff", 500, function() {
+    let main = $(".main-container");
+    clearElement(main);
+    main.show("scale", { percent: 0 }, 500, slideCanvas);
+  });
+});
+
+
+function slideCanvas() {
+  let imgContainer = $(CANVAS_CONTAINER);
+  clearElement(imgContainer);
+  imgContainer.show("slide", 500);
+  image.src = $(SELECTED_IMAGE_THUMBNAIL).attr("src");
+}
+
+function clearElement(el) {
+  el.removeClass("hidden");
+  el.hide();
+}
+
+function loadImage(fileInput) {
+  let file =  $(fileInput)[0].files[0];
 
   var reader = new FileReader();
   reader.onload = function(e) {
-      $("#selectedImage").attr("src", e.target.result);
-      image.src = e.target.result;
+      $(SELECTED_IMAGE_THUMBNAIL).attr("src", e.target.result);
       bindFilters();
   };
   reader.readAsDataURL(file);
-
-});
+}
 
 
 function getDownloadData() {
