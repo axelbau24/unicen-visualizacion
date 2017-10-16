16c04d8f7e11c0d280378776fe05e31a512e8741
Axel Bautista <axelbau24@gmail.com>
2017-10-16 01:47:56 -0300

    Agregado de fisicas básicas

--- TP4/EntregaFinal/js/game.js
+++ TP4/EntregaFinal/js/game.js
@@ -1,8 +1,65 @@
-class Player {
-  constructor(selector) {
-    this.object = selector;
-    this.x = 0;
-    this.y = 20;
+class Game {
+  constructor() {
+    this.gameObjects = [];
+    let game = this;
+    setInterval(function () { game.update(); }, 0);
+  }
+  update() {
+    for (var i = 0; i < this.gameObjects.length; i++) {
+      let gameObject = this.gameObjects[i];
+      for (var j = 0; j < this.gameObjects.length; j++) {
+        if(i != j) gameObject.checkCollision(this.gameObjects[j]);
+      }
+      gameObject.update();
+
+    }
+  }
+  addObject(object){
+    this.gameObjects.push(object);
+  }
+}
+
+class GameObject {
+  constructor(element, staticObject) {
+    this.element = element;
+    if(staticObject) this.staticObject = staticObject;
+    else this.staticObject = false;
+    this.x = parseFloat(this.getCSSProperty("left"));
+    this.y = parseFloat(this.getCSSProperty("top"));
+    this.width = parseFloat(this.getCSSProperty("width"));
+    this.height = parseFloat(this.getCSSProperty("height"));
+    this.colliding = false;
+  }
+
+  getCSSProperty(propertyName){
+    let style = window.getComputedStyle(this.element);
+    return style.getPropertyValue(propertyName);
+  }
+  update(){
+    if(!this.colliding && !this.staticObject) this.setPos(this.x, (this.y + 5.05));
+  }
+  setPos(x, y){
+    this.x = x;
+    this.y = y;
+    this.element.style.left = this.x + "px";
+    this.element.style.top = this.y + "px";
+  }
+  checkCollision(gameObject){
+    if(this.y + this.height > gameObject.y && this.x + this.width > gameObject.x &&
+       this.x < gameObject.x + gameObject.width && this.y < gameObject.y - gameObject.height){
+
+      this.colliding = true;
+    }
+    else this.colliding = false;
+  }
+
+}
+
+
+
+class Player extends GameObject{
+  constructor(element) {
+    super(element);
     this.currentEvent = null;
     this.holdingKey = false;
     let p = this;
@@ -25,17 +82,22 @@ class Player {
     this.holdingKey = false;
   }
 
-  move(){
+  update(){
+    super.update();
     if(this.holdingKey){
       if(this.currentEvent.keyCode == 68 || this.currentEvent.keyCode == 39) {
-        this.x += 5;
-        this.object.style.transform = "scaleX(1)";
+        this.x += 2.5;
+        this.element.style.transform = "scaleX(1)";
       }
       else if(this.currentEvent.keyCode == 65 || this.currentEvent.keyCode == 37){
-          this.x -= 5;
-          this.object.style.transform = "scaleX(-1)";
+          this.x -= 2.5;
+          this.element.style.transform = "scaleX(-1)";
       }
-      this.object.style.left = this.x + "px";
+      else if(this.currentEvent.keyCode == 38){
+          this.y -= 10.5;
+          this.element.style.top = this.y + "px";
+      }
+      this.element.style.left = this.x + "px";
     }
   }
 
@@ -50,17 +112,28 @@ class Player {
   }
 }
 
-let player = new Player(document.getElementById('player'));
+let game = new Game();
+
+game.addObject(new GameObject(document.getElementById('cube')));
+game.addObject(new Player(document.getElementById('player')));
+game.addObject(new GameObject(document.getElementById('floor'), true));
 
 
-setInterval(update, 10);
 
-function update() {
-  // let style = window.getComputedStyle(player.object),
-  // top = style.getPropertyValue('bottom');
-  // console.log(top);
-  player.move();
-  player.jump();
-}
 
-update();
+
+
+//let player = new Player(document.getElementById('player'));
+
+
+
+
+
+// function update() {
+//   // let style = window.getComputedStyle(player.object),
+//   // top = style.getPropertyValue('bottom');
+//   // console.log(top);
+//   //player.move();
+//   //player.jump();
+// }
+//
