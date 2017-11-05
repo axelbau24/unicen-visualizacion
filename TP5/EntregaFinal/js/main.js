f6b0cb99034354ae1eff3bcfe907234cebfef0dd
Axel Bautista <axelbau24@gmail.com>
2017-11-04 21:23:29 -0300

    Modificaciones en las vistas

--- TP5/EntregaFinal/js/main.js
+++ TP5/EntregaFinal/js/main.js
@@ -1,5 +1,6 @@
 let imagenes = [];
 let contador = 0;
+let layout = 1;
 let cb = new Codebird;
 cb.setConsumerKey("iOd7FdG7T3dE1asEHOFxLmdKr", "VdkGM4SSycjpkKrALkwbrO7aQiz696d6twGrVVRcYTvJDS00xQ");
 cb.setToken("1368012535-kTk798jfE3ySB8ObnlWCzqNJ8uydUqvFEfkTyAk", "NkBbB1mistXTu7JWC77gdCC1WMmRnNQVQarg6HHOijCca");
@@ -8,7 +9,7 @@ let finalizado = false;
 
 function llamado(hash,type){
   var params = {
-      count:50,
+      count:100,
       q: hash,
       result_type: type
   };
@@ -16,6 +17,7 @@ function llamado(hash,type){
       "search_tweets",
       params,
       function (reply) {
+        console.log(reply);
         for (var i = 0; i < reply.statuses.length; i++) {
           let tweet = reply.statuses[i];
           if(tweet.extended_entities && tweet.extended_entities.media[0].type == "photo"){
@@ -40,9 +42,23 @@ function llamado(hash,type){
 function mostrarImagenes(){
   $('.img-container').each(function(){
 
-    $(this).find(".like-count").html(imagenes[contador].likes);
-    $(this).find(".images").attr("src", imagenes[contador++].url)
-  })
+    //$(this).find(".like-count").html(imagenes[contador].likes);
+    $(this).find(".image").attr("src", imagenes[contador++].url)
+  });
+
+  $layout.imagesLoaded().progress( function() {
+    $layout.masonry();
+  });
 }
 
 llamado("#messi","popular");
+
+var $layout = $(".grid").masonry({
+  itemSelector: ".grid__item_layout_1",
+  columnWidth: ".grid__sizer",
+  gutter: 15,
+  transitionDuration: 0,
+  fitWidth: true,
+ });
+
+ 
