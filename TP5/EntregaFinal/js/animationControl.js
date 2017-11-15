ea4c87a9f829f484ef35938c0d0f6060d11fb968
MarcosCifuentes <marcosnaza16@gmail.com>
2017-11-15 17:53:14 -0300

    agregadas transiciones de imagenes

--- TP5/EntregaFinal/js/animationControl.js
+++ TP5/EntregaFinal/js/animationControl.js
@@ -32,5 +32,22 @@ function setImage(url) {
   let width = (height / theImage.height) * theImage.width;
   $(".big-image-container").css("background-image", "url(" + theImage.src + ")");
   $(".big-image").height(height);
-  $(".big-image-container").width(width);
+  let random = Math.floor(Math.random() * 3);
+
+    switch (random) {
+      case 0:
+        $(".big-image-container").width(0);
+        setTimeout(function () {
+          $(".big-image-container").width(width);
+        }, 500);
+        break;
+      case 1:
+        $(".big-image-container").width(width);
+        break;
+      case 2:
+        $(".big-image-container").addClass("slide");
+        $(".big-image-container").width(width)
+        setTimeout(function() { $('.big-image-container').removeClass('slide'); }, 2000);
+        break;
+    }
 }
